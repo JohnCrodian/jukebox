23d7ded66d121456d614999fadb0331d4e299fd6
John Crodian <johncrodian24@gmail.com>
2016-11-23 13:07:08 -0500

    Created files

---
+++ jukebox.js
@@ -0,0 +1,9 @@
+function musicPlayer (songList) {
+	this.songList = []
+	this.addSongList = function () {
+
+	}
+}
+
+var songList = ["FallOnMe.mp3", "GardeningAtNight.mp3", "GirlfriendInAComa.mp3", "NightSwimming.mp3", "Panic.mp3"];
+var jukeBox = new musicPlayer ();
