ef4b03360250848cb5f9fdc420b9c97728d6333c
John Crodian <johncrodian24@gmail.com>
2016-11-23 15:16:23 -0500

    Added audio player element to Javascript

--- jukebox.js
+++ jukebox.js
@@ -1,9 +1,33 @@
-function musicPlayer (songList) {
+function MusicPlayer (songList) {
 	this.songList = []
-	this.addSongList = function () {
-
+	this.addSongList = function (newSongList) {
+		this.songList.push(newSongList);
 	}
+	this.loadSong = function (firstSong) {
+		var audioElement = document.createElement('audio');
+		audioElement.setAttribute("controls", "controls");
+		audioElement.setAttribute('src', this.songList[3]);
+		document.body.appendChild(audioElement);
+		audioElement.play();
+	}
+	
+	// this.playFirst = function (song) {
+	// 	var playSong = document.getElementsByTagName("audio")[0];
+	// 	playSong.play();
+	// }
+	
 }
 
-var songList = ["FallOnMe.mp3", "GardeningAtNight.mp3", "GirlfriendInAComa.mp3", "NightSwimming.mp3", "Panic.mp3"];
-var jukeBox = new musicPlayer ();
+var jukeBox = new MusicPlayer ();
+
+var fallOnMe = "FallOnMe.mp3";
+var gardeningAtNight = "GardeningAtNight.mp3"; 
+var girlfriendInAComa = "GirlfriendInAComa.mp3"; 
+var nightSwimming = "NightSwimming.mp3";
+var panic = "Panic.mp3";
+
+jukeBox.addSongList(fallOnMe);
+jukeBox.addSongList(gardeningAtNight);
+jukeBox.addSongList(girlfriendInAComa);
+jukeBox.addSongList(nightSwimming);
+jukeBox.addSongList(panic);
